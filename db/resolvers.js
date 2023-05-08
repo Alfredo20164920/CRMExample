416ae4c36207b2a0a08478269ef087f5ef2922c4
Alfredo20164920 <apaz20164920@gmail.com>
2023-05-08 17:26:28 -0600

    feat: create schema and resolver to save user

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,8 +1,36 @@
 // Resolvers => Funciones de retornar valores que existen en el schema, consultar la BD y traer los datos, 
 // los nombres deben sern iguales a los definidos en el schema
+const User = require('../models/user');
+
 const resolvers = {
     Query: {
         getCourse: () => "Something"
+    },
+    Mutation: {
+        newUser: async (_, { input }, ctx) => {
+            
+            const { email, password } = input;
+            
+            // Is user register
+            const isUserExist = await User.findOne({email});
+            if (isUserExist) {
+                throw new Error('User already exist');
+            }
+
+            // Hash password
+
+
+            // Save in DB
+            try {
+                const user = new User(input);
+                user.save();
+                
+                return user;
+            } catch (error) {
+                
+            }
+
+        }
     }
 }
 
--- db/schema.js
+++ db/schema.js
@@ -8,6 +8,25 @@ const typeDefs = gql`
     type Query {
         getCourse: String
     }
+
+    type User {
+        id: ID
+        name: String
+        lastName: String
+        email: String
+        created: String
+    }
+
+    input UserInput {
+        name: String!
+        lastName: String!
+        email: String!
+        password: String!
+    }
+
+    type Mutation {
+        newUser(input: UserInput): User 
+    }
 `;
 
 module.exports = typeDefs;
